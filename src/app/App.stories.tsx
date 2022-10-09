5cfaf08eabd5ac748a0ea952e7e303c1e201c8db
Denis Shubin <denis.shubin@gmail.com>
2022-10-09 21:56:41 +0300

    init: start project Todolist and Saga

---
+++ src/app/App.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import App from './App'
+import {
+    ReduxStoreProviderDecorator
+} from '../stories/decorators/ReduxStoreProviderDecorator'
+
+export default {
+    title: 'App Stories',
+    component: App,
+    decorators: [ReduxStoreProviderDecorator]
+}
+
+export const AppBaseExample = (props: any) => {
+    return (<App demo={true}/>)
+}
